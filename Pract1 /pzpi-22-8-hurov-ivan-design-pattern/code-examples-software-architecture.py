189cd96bc9fd9b46cb7714786ddc642a75fce825
Ivan Hurov <ivan.hurov@nure.ua>
2025-04-09 20:42:49 +0300

    Update code-examples-software-architecture.py

--- Pract1 /pzpi-22-8-hurov-ivan-design-pattern/code-examples-software-architecture.py
+++ Pract1 /pzpi-22-8-hurov-ivan-design-pattern/code-examples-software-architecture.py
@@ -1,1 +1,38 @@
-
+from abc import ABC, abstractmethod 
+ 
+# Реалізація 
+class Device(ABC): 
+    @abstractmethod 
+    def turn_on(self): 
+        pass 
+ 
+    @abstractmethod 
+    def turn_off(self): 
+        pass 
+ 
+class TV(Device): 
+    def turn_on(self): 
+        print("TV is now ON") 
+ 
+    def turn_off(self): 
+        print("TV is now OFF") 
+ 
+class Radio(Device): 
+    def turn_on(self): 
+        print("Radio is now ON") 
+ 
+    def turn_off(self): 
+        print("Radio is now OFF") 
+ 
+# Абстракція 
+class RemoteControl: 
+    def __init__(self, device: Device): 
+        self.device = device 
+def press_power(self): 
+print("Power button pressed:") 
+self.device.turn_on() 
+# Тестування 
+tv_remote = RemoteControl(TV()) 
+radio_remote = RemoteControl(Radio()) 
+tv_remote.press_power() 
+radio_remote.press_power()
